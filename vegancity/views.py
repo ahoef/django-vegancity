6823270e4a6ffe7be1c5212225331055caa9f26b
steventlamb <steventlamb@gmail.com>
2012-09-24 10:42:05 -0400

    vegancity | adding a some styling

--- vegancity/views.py
+++ vegancity/views.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
+from django.template import RequestContext
 
 import models
 
 def home(request):
-    return render_to_response('vegancity/home.html')
+    return render_to_response('vegancity/home.html', context_instance=RequestContext(request))
 
 def spread(request):
-    return render_to_response('vegancity/spread.html')
+    return render_to_response('vegancity/spread.html', context_instance=RequestContext(request))
 
 def about(request):
-    return render_to_response('vegancity/about.html')
+    return render_to_response('vegancity/about.html', context_instance=RequestContext(request))
 
 def search(request):
     vendors = models.Vendor.objects.all()
     ctx = {
         'vendors' : vendors,
         }
-    return render_to_response('vegancity/search.html', ctx)
+    return render_to_response('vegancity/search.html', ctx, context_instance=RequestContext(request))
 
