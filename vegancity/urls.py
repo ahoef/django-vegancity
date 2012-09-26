8bc7e27a805fb22adf65eb80d674e1ef2abdac5f
steve <steve@Botvinnik>
2012-09-26 09:49:01 -0400

    vegancity | comments

--- vegancity/urls.py
+++ vegancity/urls.py
@@ -5,16 +5,19 @@ from settings import INSTALLED_APPS
 
 admin.autodiscover()
 
+# CUSTOM VIEWS
 urlpatterns = patterns('vegancity.views',
     url(r'^search/$', 'search', name='search'),
-)
+    )
 
+# GENERIC VIEWS
 urlpatterns += patterns('django.views.generic.simple',
     url(r'^$', 'direct_to_template', {'template': 'vegancity/home.html'}, name='home'),
     url(r'^about/$', 'direct_to_template', {'template': 'vegancity/about.html'}, name='about'),
     url(r'^spread/$','direct_to_template', {'template': 'vegancity/spread.html'}, name='spread'),
-)                        
+    )                        
 
+# ADMIN VIEWS
 if 'django.contrib.admin' in INSTALLED_APPS:
     urlpatterns += patterns('',
         url(r'^admin/', include(admin.site.urls)),
