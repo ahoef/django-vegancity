964f9f4963aff1aa8abefc5a8255960241dce25b
steve <steventlamb@gmail.com>
2012-09-26 20:57:19 -0400

    vegancity scraping | updated lex script

--- lex_google_data.py
+++ lex_google_data.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python
+
+import sys
 import ply.lex as lex
 import pprint
+
+from django.core.management import setup_environ
+
+import settings
+
+setup_environ(settings)
+
+import vegancity.models as models
+
 
 tokens = ('NAME', 'ADDRESS', 'PHONE', 'URL', 'ZAGAT', 'CATEGORY', 'TAGS', 'REVIEW')
 
@@ -26,27 +38,27 @@ def t_URL(token):
 def t_REVIEW_1(token):
     r'".*"[ ]-[^\n]*'
     token.type = 'REVIEW'
-    return token
+    return
 
 def t_REVIEW_2(token):
     r'[^\n"]*"[^\n"]+"[^\n"]*'
     token.type = 'REVIEW'
-    return token
+    return
 
 def t_REVIEW_3(token):
     r'[0-9 ]+review[^\n]*'
     token.type = 'REVIEW'
-    return token
+    return
 
 def t_REVIEW_4(token):
     r'[^\n]*reviewer[^\n]*'
     token.type = 'REVIEW'
-    return token
+    return
 
 def t_REVIEW_5(token):
     r'[^\n]*Diners\'[ ]Choice[^\n]*'
     token.type = 'REVIEW'
-    return token
+    return
 
 def t_CATEGORY(token):
     r'Category:[^\n]+'
@@ -59,7 +71,6 @@ def t_ZAGAT_STATS(token):
     return
 
 def t_TAGS(token):
-    #r'([A-Za-z0-9\' ]+[ ]:::[ ])+[^:\n]+'
     r'([A-Za-z0-9\' ]+[ ]:::[ ])+[A-Za-z0-9\' ]+'
     token.type = 'TAGS'
     token.value = token.value.split(" ::: ")
@@ -74,24 +85,43 @@ def t_NAME(token):
 t_ignore = ' \t\v\r'
 
 def t_error(t):
-  print "Lexer: unexpected character " + t.value[0]
   t.lexer.skip(1) 
 
-lexer = lex.lex() 
-
-def run_lexer(input_string):
-  lexer.input(input_string)
-  result = [ ] 
-  while True:
-    tok = lexer.token()
-    if not tok: break
-    result = result + [(tok.type, tok.value)]
-  return result
-
 def main():
-    f = open('more_raw_philly_data.txt', 'r')
+
+    try:
+        f = open(sys.argv[1], 'r')
+    except IOError as e:
+        print 'FILE NOT FOUND.'
+        sys.exit()
+    
     raw_string = f.read()
-    results = (run_lexer(raw_string))
+    lexer = lex.lex() 
+    lexer.input(raw_string)
+    result = [ ] 
+    while True:
+        tok = lexer.token()
+        if not tok: break
+        result = result + [(tok.type, tok.value)]
+
+    pprint.pprint(result)
+    name_indices = [i for i, v in enumerate(result) if v[0] == 'NAME']
+    ranges = [range(name_indices[i], name_indices[i+1]) for i in range(len(name_indices) - 1)] + [range(name_indices[-1], len(result))]
+    tuples_lists = (map(lambda x: map(lambda y: result[y], x), ranges))
+    hashes = map(dict, tuples_lists)
+    pprint.pprint(hashes)
+    
+    
+    for hash in hashes:
+        vendor = models.Vendor()
+        vendor.name = hash['NAME']
+        vendor.address =  hash['ADDRESS']
+        vendor.phone = hash['PHONE']
+        vendor.url = hash['URL']
+        for tag in hash['TAGS']:
+            # add a tag for that vendor
+            pass
+        vendor.save()
     
 if __name__ == '__main__':
     main()
