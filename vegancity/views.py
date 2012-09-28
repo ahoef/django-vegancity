90d5e9309d53743beff0ff5236ad02e3b3117757
steve <steventlamb@gmail.com>
2012-09-28 14:42:41 -0400

    search | adding a *very* basic search feature to navbar

--- vegancity/views.py
+++ vegancity/views.py
@@ -5,7 +5,16 @@ from django.template import RequestContext
 import models
 
 def vendors(request):
-    vendors = models.Vendor.objects.all()
+
+    if request.method == 'POST':
+        raise AssertionError, "this shouldn't happen!"
+
+    if request.GET:
+        vendors = models.Vendor.objects.filter(name__icontains=request.GET['query'])
+    
+    else:
+        vendors = models.Vendor.objects.all()
+
     ctx = {
         'vendors' : vendors,
         }
@@ -20,3 +29,11 @@ def vendor_detail(request, vendor_id):
         }
     return render_to_response('vegancity/vendor_detail.html', ctx, context_instance=RequestContext(request))
 
+def new_vendor(request):
+    if request.method == 'GET':
+        # the person is visiting the page for the first time
+        pass
+
+    elif request.method == 'POST':
+        # the person just posted some data
+        pass
