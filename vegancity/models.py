00e748a5009448ee84a1bc83d5fd105492a69665
steve <steventlamb@gmail.com>
2012-10-03 16:55:31 -0400

    search | added manager class for vendors.
    Keep the views simple, put the complexity in the model.

--- vegancity/models.py
+++ vegancity/models.py
@@ -1,16 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 import geocode
+import shlex
 
 TAG_LIST = (
-    # # commenting these tags out because we probably stick
-    # # with a dedicated DB field.
-    # ('vegan', "100% Vegan"),
-    # ('veg_mostly_vegan', "Vegetarian - Mostly Vegan"),
-    # ('veg_hardly_vegan', "Vegetarian - Hardly Vegan"),
-    # ('not_veg', "Not Vegetarian"),
-    # ('beware', "Beware!"),
     ('chinese', 'Chinese'),
     ('delivery','Offers Delivery'),
     ('open_late', 'Open after 10pm'),
@@ -39,6 +34,73 @@ class Tag(models.Model):
     def __unicode__(self):
         return self.name
 
+class VendorManager(models.Manager):
+    "Manager class for handling searches by vendor."
+
+    def tags_search(self, query):
+        """Search vendors by tag.
+
+Takes a query, breaks it into tokens, searches for tags
+that contain the token.  If any of the tokens match any
+tags, return all the orgs with that tag."""
+        tokens = shlex.split(query)
+        q_builder = Q()
+        for token in tokens:
+            q_builder = q_builder | Q(name__icontains=token)
+        tag_matches = Tag.objects.filter(q_builder)
+        vendors = set()
+        for tag in tag_matches:
+            for vendor in tag.vendor_set.all():
+                vendors.add(vendor)
+        vendor_count = len(vendors)
+        summary_string = ('Found %d results with tags matching "%s".' 
+                          % (vendor_count, ", ".join(tokens)))
+        return {
+            'count' : vendor_count, 
+            'summary_statement' : summary_string, 
+            'vendors':vendors
+            }
+
+    def name_search(self, query):
+        """Search vendors by name.
+
+Takes a query, breaks it into tokens, searches for names
+that contain the token.  If any of the tokens match any
+names, return all the orgs with that name."""
+        tokens = shlex.split(query)
+        q_builder = Q()
+        for token in tokens:
+            q_builder |= Q(name__icontains=token)
+        vendors = self.filter(q_builder)
+        vendor_count = vendors.count()
+        summary_string = ('Found %d results where name contains "%s".' 
+                          % (vendor_count, " or ".join(tokens)))
+        return {
+            'count' : vendor_count,
+            'summary_statement' : summary_string, 
+            'vendors' : vendors
+            }
+
+    #TODO - replace with something better!
+    def address_search(self, query):
+        """ Search vendors by address.
+
+THIS WILL BE CHANGED SO NOT WRITING DOCUMENTATION."""
+        tokens = shlex.split(query)
+        q_builder = Q()
+        for token in tokens:
+            q_builder |= Q(address__icontains=token)
+        vendors = self.filter(q_builder)
+        vendor_count = vendors.count()
+        summary_string = ('Found %d results where address contains "%s".' 
+                          % (vendor_count, " or ".join(tokens)))
+        return {
+            'count' : vendor_count, 
+            'summary_statement' : summary_string, 
+            'vendors':vendors
+            }
+        
+
 class Vendor(models.Model):
     "The main class for this application"
     name = models.CharField(max_length=200)
@@ -46,12 +108,16 @@ class Vendor(models.Model):
     phone = models.CharField(max_length=50)
     website = models.URLField()
     notes = models.TextField(blank=True, null=True,)
-    veg_level = models.IntegerField(choices=VEG_LEVELS, blank=True, null=True,)
-    food_rating = models.IntegerField(choices=RATINGS, blank=True, null=True, )
-    atmosphere_rating = models.IntegerField(choices=RATINGS, blank=True, null=True,)
+    veg_level = models.IntegerField(choices=VEG_LEVELS, 
+                                    blank=True, null=True,)
+    food_rating = models.IntegerField(choices=RATINGS, 
+                                      blank=True, null=True, )
+    atmosphere_rating = models.IntegerField(choices=RATINGS, 
+                                            blank=True, null=True,)
     latitude = models.FloatField(default=None, blank=True, null=True)
     longitude = models.FloatField(default=None, blank=True, null=True)
     tags = models.ManyToManyField(Tag, null=True, blank=True)
+    objects = VendorManager()
 
     def __unicode__(self):
         return self.name
@@ -64,6 +130,7 @@ class Vendor(models.Model):
         super(Vendor, self).save(*args, **kwargs)
 
 
+    
 class Review(models.Model):
     entry_date = models.DateTimeField()
     vendor = models.ForeignKey('Vendor')
