2f60a79a9c9f6da9da0646c146898a309ef1db13
steve <steventlamb@gmail.com>
2012-10-09 01:44:05 -0400

    admin | updating admin managers to make workflows easier

--- vegancity/admin.py
+++ vegancity/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from models import Vendor, Review, QueryString, BlogEntry, VeganDish, CuisineTag, FeatureTag
 
-admin.site.register(Vendor)
-admin.site.register(Review)
+# vendors
+class VendorAdmin(admin.ModelAdmin):
+    "Make it easier to admin the vendors"
+    list_display = ('id','approved', 'name', 'entry_date')
+    list_filter = ('approved',)
+
+admin.site.register(Vendor, VendorAdmin)
+
+# reviews
+class ReviewAdmin(admin.ModelAdmin):
+    "Make it easier to admin the reviews"
+    list_display = ('id', 'approved', 'vendor',)
+    list_filter = ('approved', 'best_vegan_dish', 'unlisted_vegan_dish')
+
+admin.site.register(Review, ReviewAdmin)
+
+# other
 admin.site.register(QueryString)
 admin.site.register(BlogEntry)
 admin.site.register(VeganDish)
