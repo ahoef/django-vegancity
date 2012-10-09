5c43c1e10e44df3d44794517b5cf63d7eedb2820
steve <steventlamb@gmail.com>
2012-10-08 21:47:12 -0400

    admin | add VeganDish to admin

--- vegancity/admin.py
+++ vegancity/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from models import Vendor, Review, QueryString, Tag, BlogEntry
+from models import Vendor, Review, QueryString, Tag, BlogEntry, VeganDish
 
 admin.site.register(Vendor)
 admin.site.register(Review)
 admin.site.register(QueryString)
 admin.site.register(Tag)
 admin.site.register(BlogEntry)
+admin.site.register(VeganDish)
 
 
