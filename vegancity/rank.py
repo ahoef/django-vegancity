4c33eead92ef5cd875d86e13bd2364be8a737851
steve <steventlamb@gmail.com>
2012-10-10 16:05:07 -0400

    license | adding gplv3 :)

--- vegancity/rank.py
+++ vegancity/rank.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+
+# Copyright (C) 2012 Steve Lamb
+
+# This file is part of Vegancity.
+
+# Vegancity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Vegancity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Vegancity.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 # A module for taking a search query and deciding which resultset to show first.
 
 # The idea is that queries with certain patterns are likely to be lumped as an
