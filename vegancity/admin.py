98c4f5ac9698528297024352847407b22e625ea2
steve <steventlamb@gmail.com>
2012-10-21 15:40:51 -0400

    admin | ugh, bringing back admin.py

--- vegancity/admin.py
+++ vegancity/admin.py
@@ -16,50 +16,26 @@
 # along with Vegancity.  If not, see <http://www.gnu.org/licenses/>.
 
 
+from django.contrib import admin
+import models
 
+class ReviewAdmin(admin.ModelAdmin):
+        list_display = ('vendor', 'approved',)
+        list_filter = ('approved', 'best_vegan_dish', 'unlisted_vegan_dish')
 
+class VendorAdmin(admin.ModelAdmin):
+        list_display = ('id','approved', 'name', 'created', 'neighborhood')
+        list_filter = ('approved',)
 
-############################################################
-############################################################
-############################################################
+#####################################
+## ADMIN REGISTRATION
+#####################################
 
-# This file has been deprecated.
-
-# Will be removed from future release.
-
-# To configure admin options, please see models.py
-
-
-############################################################
-############################################################
-############################################################
-
-
-
-# from django.contrib import admin
-# import models
-
-# # vendors
-# class VendorAdmin(admin.ModelAdmin):
-#     "Make it easier to admin the vendors"
-#     list_display = ('id','approved', 'name', 'created')
-#     list_filter = ('approved',)
-
-# admin.site.register(models.Vendor, VendorAdmin)
-
-# # reviews
-# class ReviewAdmin(admin.ModelAdmin):
-#     "Make it easier to admin the reviews"
-#     list_display = ('id', 'approved', 'vendor',)
-#     list_filter = ('approved', 'best_vegan_dish', 'unlisted_vegan_dish')
-
-# admin.site.register(models.Review, ReviewAdmin)
-
-# # other
-# admin.site.register(models.QueryString)
-# admin.site.register(models.BlogEntry)
-# admin.site.register(models.VeganDish)
-# admin.site.register(models.CuisineTag)
-# admin.site.register(models.FeatureTag)
-# admin.site.register(models.Neighborhood)
-
+admin.site.register(models.Vendor, VendorAdmin)
+admin.site.register(models.Review, ReviewAdmin)
+admin.site.register(models.QueryString)
+admin.site.register(models.BlogEntry)
+admin.site.register(models.VeganDish)
+admin.site.register(models.CuisineTag)
+admin.site.register(models.FeatureTag)
+admin.site.register(models.Neighborhood)
