f237210460b6d5f82794123640450f6ef969586e
steve <steventlamb@gmail.com>
2013-02-23 02:30:52 -0500

    Fix up build tools

--- Vagrantfile
+++ Vagrantfile
@@ -4,18 +4,16 @@ Vagrant::Config.run do |config|
   config.vm.box = "precise32"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
 
+  # disable this as needed
   config.vm.network :bridged
 
   # Forward guest port 80 to host port 8080
   config.vm.forward_port 80, 8080
-  #this is no longer needed because we are giving
-  #this vm its own IP on the local network.
-  #config.vm.forward_port 8000, 8000
 
   # Share Host "~/projects" with guest "/var/projects/share"
   config.vm.share_folder "share", "/var/projects/vegphilly", "."
 
-  # this is still way too much of a wip
-  #config.vm.provision :shell, :path => "build.sh"
+  # run provisioner script from utils
+  config.vm.provision :shell, :path => "utils/spin_up/build.sh"
 
 end
--- utils/spin_up/build.sh
+++ utils/spin_up/build.sh
@@ -4,22 +4,19 @@
 # Ideally should be run only in conjunction
 # with a vagrant initialization.
 
-sudo apt-get update
-# sorry for hogging bandwidth
-# but the vegphilly team needs
-# emacs to do everything.
+apt-get update
 # TODO: don't require emacs.
-sudo apt-get install emacs
-sudo apt-get install -y postgresql
-sudo apt-get install -y memcached
+apt-get install -y emacs
+apt-get install -y postgresql
+#apt-get install -y memcached
 
-sudo apt-get install -y nginx
-sudo cp -v /var/projects/vegphilly/utils/nginx_vegphilly.conf /etc/nginx/conf.d/
-sudo service nginx restart
+#sudo apt-get install -y nginx
+#sudo cp -v /var/projects/vegphilly/utils/nginx_vegphilly.conf /etc/nginx/conf.d/
+#sudo service nginx restart
 
-sudo apt-get install -y python-pip
+apt-get install python-pip
 
-sudo pip install django
-sudo pip install south
-sudo pip install gunicorn
+pip install Django
+pip install south
 
+apt-get install -y python-psycopg2
