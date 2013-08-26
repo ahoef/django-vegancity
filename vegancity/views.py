ahoef/django-vegancity

commit 37238f61dbe97de54789dd7fe3f2dead538f362e

Don't use a form class for the search page
This form class was complicated with no benefit. As a start (and maybe
a finish) move this logic to a view function.

templates/vegancity/vendors.html
@@ -14,10 +14,10 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js"></script>
   <script type="text/javascript">
 
-    var autoResize = {% if search_form.has_get_params %}true{% else %}false{% endif %};
-    var defaultCenter = new google.maps.LatLng({{ search_form.center_latitude }}, {{ search_form.center_longitude }});
+    var autoResize = {% if has_get_params %}true{% else %}false{% endif %};
+    var defaultCenter = new google.maps.LatLng({{ center_latitude }}, {{ center_longitude }});
     var vendors = [
-    {% for vendor in search_form.vendors %}{% if vendor.location %}
+    {% for vendor in vendors %}{% if vendor.location %}
         {
             id: "{{ vendor.id }}",
             name: "{{ vendor.name }}",
@@ -48,20 +48,20 @@
     <div class="span6">
 
       <form action="{% url 'vendors' %}" method="get" name="filters" id="filters" class="form-search">
-        {% if search_form.search_type %}
+        {% if search_type %}
           {% comment %}If there's a search_type, stick it in the form, otherwise dont.
 This prevents null searches from getting a value of None stuck here and throwing off the UI.{% endcomment %}            
-        <input id="search_type" name="search_type" type="hidden" value="{{ search_form.search_type }}">
+        <input id="search_type" name="search_type" type="hidden" value="{{ search_type }}">
       {% endif %}        
         {% if search_from.old_query %}
-        <input id="old_query" name="old_query" type="hidden" value="{{ search_form.old_query }}">
+        <input id="previous_query" name="previous_query" type="hidden" value="{{ previous_query }}">
       {% endif %}
         <input id="search-input" type="text" class="input-large" 
-               autofocus="autofocus" autocomplete="off" name="query" 
-               {% if search_form.query %}value="{{ search_form.query }}"{% endif %}>
-                 {% if search_form.search_type %}
+               autofocus="autofocus" autocomplete="off" name="current_query" 
+               {% if current_query %}value="{{ current_query }}"{% endif %}>
+                 {% if search_type %}
                <div class="btn-group">
-                 <button class="btn btn-custom" id="search_by_dynamic" title="{{ search_form.search_type|format_button_title }}">Search by {{ search_form.search_type|format_search_type }}</button>
+                 <button class="btn btn-custom" id="search_by_dynamic" title="{{ search_type|format_button_title }}">Search by {{ search_type|format_search_type }}</button>
                  <button class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
                    <span class="caret"></span>
                  </button>
@@ -76,12 +76,12 @@ This prevents null searches from getting a value of None stuck here and throwing
                {% endif %}
                  <a href="#" class="btn btn-warning" id="clear_search">Clear Search</a>
         <br>
-        {% if search_form.query %}
-        <h5>Showing {% if search_form.vendors %}only{% else %}no{% endif %} results{{ search_form.search_type|showing_vendors_string }}{% if search_form.query %}"{{ search_form.query }}"{% endif %}</h5>
+        {% if current_query %}
+        <h5>Showing {% if vendors %}only{% else %}no{% endif %} results{{ search_type|showing_vendors_string }}{% if current_query %}"{{ current_query }}"{% endif %}</h5>
       {% else %}
         <br>
       {% endif %}
-      {% if search_form.vendors %}
+      {% if vendors %}
         <div id="map_canvas"></div>
       {% else %}
         <div id="map_canvas" style="visibility: hidden"></div>
@@ -93,18 +93,36 @@ This prevents null searches from getting a value of None stuck here and throwing
         <div class="span3">
 
             <h4>Neighborhood:</h4>
-              {{ search_form.neighborhood }}
+              <select id="id_neighborhood" name="neighborhood">
+                <option value="" selected="selected">---------</option>
+                {% for neighborhood in neighborhoods %}
+                <option value="{{ neighborhood.id }}">{{ neighborhood.name }}</option>
+                {% endfor %}
+              </select>
             <h4>Cuisine:</h4>
-            {{ search_form.cuisine }}
+              <select id="id_cuisine" name="cuisine_tag">
+                <option value="" {% if selected_cuisine_tag %}selected="selected"{% endif %}>---------</option>
+                {% for cuisine_tag in cuisine_tags %}
+                <option value="{{ cuisine_tag.id }}" {% if cuisine_tag == selected_cuisine_tag %}selected="selected"{% endif %}>{{ cuisine_tag.description }}</option>
+                {% endfor %}
+              </select>
+
             <h4>Features:</h4>
-            {{ search_form.checked_features }}
-            {% for checked in search_form.checked_feature_filters %}
+
+            {% for checked in checked_feature_filters %}
               <label class="checkbox">
                 <input type="checkbox" id="id_feature_{{ checked.name }}" name="{{ checked.name }}" value="True" checked="checked" onchange="this.form.submit()">
                 {{ checked }}
               </label>
             {% endfor %}         
-            {{ search_form.feature }}
+              <select id="id_feature" name="feature_tag">
+                <option value="" {% if selected_feature_tag %}selected="selected"{% endif %}>---------</option>
+                {% for feature_tag in feature_tags %}
+                <option value="{{ feature_tag.id }}">
+                  {{ feature_tag.description }}
+                </option>
+                {% endfor %}
+              </select>
 
           </form>
 
@@ -130,11 +148,11 @@ This prevents null searches from getting a value of None stuck here and throwing
             <thead>
               <tr>
                 <th>Map</th>
-                <th>Results ({{ search_form.vendor_count|default:"0" }})</th>
+                <th>Results ({{ vendor_count|default:"0" }})</th>
               </tr>
             </thead>
             <tbody>
-              {% for vendor in search_form.vendors %}
+              {% for vendor in vendors %}
                 <tr>
                   <td><img src="" class="marker-link vendor-{{vendor.id}} veg-level-{{vendor.veg_level.id|default:"0"}}"></td>
                   <td><a class="blue" href="{{ vendor.get_absolute_url }}">{{ vendor.name }}</a></td>

vegancity/views.py
@@ -26,11 +26,13 @@ from django.db.models import Max, Count, Avg
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.views.generic import DetailView, TemplateView
+from django.conf import settings
 
 import django.contrib.auth.views
 
 from vegancity import forms
 from vegancity import models
+from vegancity import search
 
 
 def password_change(request):
@@ -110,16 +112,55 @@ def user_profile(request, username):
 
 
 def vendors(request):
-    """Display table level data about vendors.
+    center_latitude, center_longitude = settings.DEFAULT_CENTER
+    current_query = request.GET.get('current_query', None)
+    previous_query = request.GET.get('previous_query', None)
+    search_type = request.GET.get('search_type', None)
+    selected_neighborhood = request.GET.get('neighborhood', None)
+    selected_cuisine_tag_id = request.GET.get('cuisine_tag', None)
+    selected_feature_tag_id = request.GET.get('feature_tag', None)
+    checked_feature_filters = [f for f
+                               in models.FeatureTag.objects.with_vendors()
+                               if request.GET.get(f.name) or
+                               selected_feature_tag_id == str(f.id)]
 
-    If this view has a get param called query, then we trigger
-    the search runmode.  Otherwise, we just return all vendors
-    in our database."""
+    vendors = models.Vendor.approved_objects.all()
 
-    search_form = forms.SearchForm(request.GET)
+    if selected_neighborhood:
+        vendors = vendors.filter(neighborhood__id=selected_neighborhood)
 
-    return render_to_response('vegancity/vendors.html',
-                              {'search_form': search_form},
+    if selected_cuisine_tag_id:
+        selected_cuisine_tag = models.CuisineTag.objects.get(
+            pk=selected_cuisine_tag_id)
+        vendors = vendors.filter(cuisine_tags__id=selected_cuisine_tag_id)
+    else:
+        selected_cuisine_tag = None
+
+    if selected_feature_tag_id:
+        vendors = vendors.filter(feature_tags__id=selected_feature_tag_id)
+
+    for f in checked_feature_filters:
+        vendors = vendors.filter(feature_tags__id__exact=f.id)
+
+    vendors, search_type = search.filter_vendors_by_search(
+        vendors, current_query, search_type)
+
+    ctx = {
+        'cuisine_tags': models.CuisineTag.objects.all(),
+        'feature_tags': models.FeatureTag.objects.all(),
+        'neighborhoods': models.Neighborhood.objects.with_vendors(),
+        'vendor_count': len(vendors),
+        'vendors': vendors,
+        'previous_query': previous_query,
+        'current_query': current_query,
+        'selected_cuisine_tag': selected_cuisine_tag,
+        'checked_feature_filters': checked_feature_filters,
+        'search_type': search_type,
+        'has_get_params': len(request.GET) > 0,
+        'center_latitude': center_latitude,
+        'center_longitude': center_longitude,
+    }
+    return render_to_response('vegancity/vendors.html', ctx,
                               context_instance=RequestContext(request))
 
 

vegancity/forms.py
@@ -18,9 +18,7 @@
 
 from django import forms
 
-from settings import DEFAULT_CENTER
 import models
-import search
 
 from django.contrib.auth.forms import UserCreationForm
 
@@ -186,95 +184,3 @@ class NewReviewForm(_BaseReviewForm):
         widgets = {
             'vendor': forms.HiddenInput,
         }
-
-
-class SearchForm(forms.Form):
-
-    neighborhood = forms.ModelChoiceField(queryset=models.Neighborhood
-                                          .objects
-                                          .distinct(),
-                                          required=False)
-    cuisine = forms.ModelChoiceField(queryset=models.CuisineTag.objects
-                                     .with_vendors()
-                                     .distinct(),
-                                     required=False)
-    feature = forms.ModelChoiceField(queryset=models.FeatureTag.objects
-                                     .with_vendors()
-                                     .distinct(),
-                                     required=False)
-
-    def __init__(self, *args, **kwargs):
-        super(SearchForm, self).__init__(*args, **kwargs)
-
-        # initialize extra values
-        self.selected_neighborhood = self.data.get('neighborhood', None)
-        self.selected_cuisine = self.data.get('cuisine', None)
-        self.selected_feature = self.data.get('feature', None)
-        self.query = self.data.get('query', None)
-        # TODO: use this later
-        self.old_query = self.data.get('old_query', None)
-        self.search_type = self.data.get('search_type', None)
-        self.vendors = None
-        self.has_get_params = (True if self.data else False)
-        self.center_latitude, self.center_longitude = DEFAULT_CENTER
-
-        self.checked_feature_filters = []
-        for f in models.FeatureTag.objects.with_vendors():
-            if self.data.get(f.name) or self.selected_feature == str(f.id):
-                self.checked_feature_filters.append(f)
-
-        if self.is_valid():
-            self.apply_search()
-            self.filter_selections_by_vendors(self.vendors)
-
-    def apply_search(self):
-        if self.query:
-            if self.search_type == 'name':
-                self.vendors, _ = search.name_search(
-                    self.query, self.get_pre_filtered_vendors())
-            elif self.search_type == 'address':
-                self.vendors, _ = search.address_search(
-                    self.query, self.get_pre_filtered_vendors())
-            elif self.search_type == 'tag':
-                self.vendors, _ = search.tag_search(
-                    self.query, self.get_pre_filtered_vendors())
-            else:
-                self.vendors, self.search_type = search.master_search(
-                    self.query, self.get_pre_filtered_vendors())
-        else:
-            self.vendors = self.get_pre_filtered_vendors()
-
-        # TODO: yikes, I coded myself into a corner here! Fix it!
-        if type(self.vendors) == list or type(self.vendors) == set:
-            self.vendor_count = len(self.vendors)
-        else:
-            self.vendor_count = self.vendors.count()
-
-    def filter_selections_by_vendors(self, vendors):
-        ids = [vendor.id for vendor in vendors]
-        # don't filter these! Causes too many UI bugs!
-        # self.fields['neighborhood'].queryset = models.Neighborhood\
-        #    .objects.filter(vendor__in=ids).distinct()
-        # self.fields['cuisine'].queryset = models.CuisineTag\
-        #    .objects.filter(vendor__in=ids).distinct()
-        self.fields['feature'].queryset = models.FeatureTag\
-                                                .objects\
-                                                .filter(vendor__in=ids)\
-                                                .distinct()
-
-    def get_pre_filtered_vendors(self):
-        vendors = models.Vendor.approved_objects.all()
-
-        for f in self.checked_feature_filters:
-            vendors = vendors.filter(feature_tags__id__exact=f.id)
-
-        if self.selected_neighborhood:
-            vendors = vendors.filter(
-                neighborhood__id=self.selected_neighborhood)
-
-        if self.selected_cuisine:
-            vendors = vendors.filter(cuisine_tags__id=self.selected_cuisine)
-
-        if self.selected_feature:
-            vendors = vendors.filter(feature_tags__id=self.selected_feature)
-        return vendors

vegancity/search.py
@@ -184,3 +184,22 @@ def perform_address_search(initial_queryset, query):
         vendors = Vendor.objects.filter(location__dwithin=(point, .004))
 
         return vendors
+
+
+def filter_vendors_by_search(initial_vendors, query, search_type=None):
+        search_map = {
+            'name': name_search,
+            'address': address_search,
+            'tag': tag_search,
+        }
+        if query:
+            search_fn = search_map.get(search_type, None)
+            if search_fn:
+                vendors, _ = search_fn(query, initial_vendors)
+            else:
+                vendors, search_type = master_search(query,
+                                                     initial_vendors)
+        else:
+            vendors = initial_vendors
+
+        return vendors, search_type

vegancity/tests.py
@@ -5,7 +5,7 @@ from mock import MagicMock
 from django.db import IntegrityError
 
 from vegancity import models, views, email
-
+from bs4 import BeautifulSoup
 
 def get_user():
     user = models.User(username="Moby")
@@ -279,3 +279,55 @@ class VendorVeganDishValidationTest(TestCase):
         self.assertRaises(IntegrityError,
                           self.vendor.vegan_dishes.remove,
                           self.vegan_dish1)
+
+class SearchTest(TestCase):
+    def setUp(self):
+        self.v1 = models.Vendor.objects.create(name="Test Vendor Foo", approval_status='approved')
+        self.v2 = models.Vendor.objects.create(name="Test Vendor Bar", approval_status='approved')
+        self.v3 = models.Vendor.objects.create(name="Test Vendor Baz", approval_status='approved')
+        self.v4 = models.Vendor.objects.create(name="Test Vendor Bart", approval_status='approved')
+        self.factory = RequestFactory()
+
+    def test_search_by_name_for_substring(self):
+        request = self.factory.get('',
+                                   {'current_query': 'Bar',})
+
+        response = views.vendors(request)
+        self.assertEqual(response.content.count("Results (2)"), 1)
+
+        request = self.factory.get('',
+                                   {'current_query': 'Vendor',})
+
+        response = views.vendors(request)
+        self.assertEqual(response.content.count("Results (4)"), 1)
+
+    def test_search_by_name_approved_only(self):
+        self.v4.approval_status = 'quarantined'
+        self.v4.save()
+
+        request = self.factory.get('',
+                                   {'current_query': 'Vendor',})
+
+        response = views.vendors(request)
+        self.assertEqual(response.content.count("Results (3)"), 1)
+
+    def test_neighborhoods_field_is_populated(self):
+        """ This test was born out of an actual observed bug """
+
+        def count_option_elements():
+            request = self.factory.get('')
+            response = views.vendors(request)
+            content = BeautifulSoup(response.content)
+            neighborhood_element = filter(lambda x: x['name'] == 'neighborhood',
+                                          content.find_all('select'))[0]
+            return len(neighborhood_element.find_all('option'))
+
+        n1 = models.Neighborhood.objects.create(name="Foo")
+
+        self.assertEqual(count_option_elements(), 1)
+
+        self.v1.neighborhood = n1
+        self.v1.save()
+
+        self.assertEqual(count_option_elements(), 2)
+

commit e26e1114e498e3da0f2e2e1a43cd74198686a583

Fix neighborhood dropdown to not always be blank
This bug was introduced in 37238f61dbe97de54789dd7fe3f2dead538f362e

Did not completely populate all necessary hooks in the template to
make the neighborhood dropdown work correctly.

templates/vegancity/vendors.html
@@ -94,9 +94,9 @@ This prevents null searches from getting a value of None stuck here and throwing
 
             <h4>Neighborhood:</h4>
               <select id="id_neighborhood" name="neighborhood">
-                <option value="" selected="selected">---------</option>
+                <option value="" {% if not selected_neighborhood_id %}selected="selected"{% endif %}>---------</option>
                 {% for neighborhood in neighborhoods %}
-                <option value="{{ neighborhood.id }}">{{ neighborhood.name }}</option>
+                <option value="{{ neighborhood.id }}" {% if neighborhood.id == selected_neighborhood_id %}selected="selected"{% endif %}>{{ neighborhood.name }}</option>
                 {% endfor %}
               </select>
             <h4>Cuisine:</h4>

vegancity/views.py
@@ -119,9 +119,9 @@ def vendors(request):
     current_query = request.GET.get('current_query', None)
     previous_query = request.GET.get('previous_query', None)
     search_type = request.GET.get('search_type', None)
-    selected_neighborhood = request.GET.get('neighborhood', None)
     selected_cuisine_tag_id = request.GET.get('cuisine_tag', None)
     selected_feature_tag_id = request.GET.get('feature_tag', None)
+    selected_neighborhood_id = request.GET.get('neighborhood', '')
     checked_feature_filters = [f for f
                                in models.FeatureTag.objects.with_vendors()
                                if request.GET.get(f.name) or
@@ -129,8 +129,8 @@ def vendors(request):
 
     vendors = models.Vendor.approved_objects.all()
 
-    if selected_neighborhood:
-        vendors = vendors.filter(neighborhood__id=selected_neighborhood)
+    if selected_neighborhood_id:
+        vendors = vendors.filter(neighborhood__id=selected_neighborhood_id)
 
     if selected_cuisine_tag_id:
         selected_cuisine_tag = models.CuisineTag.objects.get(
@@ -167,6 +167,8 @@ def vendors(request):
         'previous_query': previous_query,
         'current_query': current_query,
         'selected_cuisine_tag': selected_cuisine_tag,
+        'selected_neighborhood_id': (int(selected_neighborhood_id)
+                                     if selected_neighborhood_id else None),
         'checked_feature_filters': checked_feature_filters,
         'search_type': search_type,
         'has_get_params': len(request.GET) > 0,