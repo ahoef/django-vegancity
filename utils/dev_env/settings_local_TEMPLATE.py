a7740a03f974ebf87c9cca993497e354cffdfe04
steventlamb <steventlamb@gmail.com>
2013-09-04 01:34:18 -0400

    Add debug_toolbar to development environment

--- utils/dev_env/settings_local_TEMPLATE.py
+++ utils/dev_env/settings_local_TEMPLATE.py
@@ -9,5 +9,27 @@ DATABASES = {
     }
 }
 
+DEVELOPMENT_APPS = (
+    'debug_toolbar',
+)
+
+DEVELOPMENT_MIDDLEWARE_CLASSES = (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
 EMAIL_HOST_USER = 'foo'
 EMAIL_HOST_PASSWORD = 'bar'
