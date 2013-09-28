a0873ce9686f1cfaa318901dd897e57bee3e3390
steve <steventlamb@gmail.com>
2013-09-27 22:50:34 -0400

    Make provision_vagrant more pythonic

--- utils/provision_vagrant.py
+++ utils/provision_vagrant.py
@@ -2,21 +2,25 @@
 
 import os
 
-for package in ["python-software-properties", "ansible"]:
-    command_template = "dpkg -l | grep 'ii  %s '"
-    return_code = os.system(command_template % package)
+def provision_vagrant():
+    for package in ["python-software-properties", "ansible"]:
+        command_template = "dpkg -l | grep 'ii  %s '"
+        return_code = os.system(command_template % package)
 
-    if return_code != 0:
-        # if any of the packages aren't installed,
-        # install them all and break the loop
-        os.system("apt-get update")
-        os.system("apt-get install -y python-software-properties")
-        os.system("apt-add-repository -y ppa:rquillo/ansible")
-        os.system("apt-get update")
-        os.system("apt-get install -y ansible")
-        os.system("echo localhost > /etc/ansible/hosts")
-        break
+        if return_code != 0:
+            # if any of the packages aren't installed,
+            # install them all and break the loop
+            os.system("apt-get update")
+            os.system("apt-get install -y python-software-properties")
+            os.system("apt-add-repository -y ppa:rquillo/ansible")
+            os.system("apt-get update")
+            os.system("apt-get install -y ansible")
+            os.system("echo localhost > /etc/ansible/hosts")
+            break
 
-os.system("ansible-playbook --connection=local --user=vagrant --sudo "
-          "--extra-vars is_dev_server=True "
-          "/usr/local/vegphilly/ansible/appservers.yml")
+            os.system("ansible-playbook --connection=local --user=vagrant "
+                      "--sudo --extra-vars is_dev_server=True "
+                      "/usr/local/vegphilly/ansible/appservers.yml")
+
+if __name__ == '__main__':
+    provison_vagrant()
