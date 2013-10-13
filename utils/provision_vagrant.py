5f48ec5fbeeb762f51c55dcbf5b3ae244cd8e643
steve <steventlamb@gmail.com>
2013-10-13 13:52:50 -0400

    Make provision_vagrant more readable
    I ran into this problem where I'd make changes to this file and be
    surprised that certain steps did/didn't run at different times based on
    indentation bugs. Breaking the indentation sensitive loop to a function
    will compartmentalize this problem.

--- utils/provision_vagrant.py
+++ utils/provision_vagrant.py
@@ -2,14 +2,14 @@
 
 import os
 
-def provision_vagrant():
-    for package in ["python-software-properties", "ansible"]:
+REQUIRED_PACKAGES = ["python-software-properties", "ansible"]
+
+def install_dependencies():
+    for package in REQUIRED_PACKAGES:
         command_template = "dpkg -l | grep 'ii  %s '"
         return_code = os.system(command_template % package)
 
         if return_code != 0:
-            # if any of the packages aren't installed,
-            # install them all and break the loop
             os.system("apt-get update")
             os.system("apt-get install -y python-software-properties")
             os.system("apt-add-repository -y ppa:rquillo/ansible")
@@ -17,6 +17,7 @@ def provision_vagrant():
             os.system("apt-get install -y ansible")
             break
 
+def run_ansible():
     os.system("echo [dev_servers] > /etc/ansible/hosts")
     os.system("echo localhost >> /etc/ansible/hosts")
 
@@ -24,4 +25,5 @@ def provision_vagrant():
               "/usr/local/vegphilly/ansible/site.yml")
 
 if __name__ == '__main__':
-    provision_vagrant()
+    install_dependencies()
+    run_ansible()
