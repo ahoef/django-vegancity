7eb7b204f1a12612c93be8ce32f6dfb464445a27
steve <steventlamb@gmail.com>
2013-10-13 14:57:58 -0400

    Explicitly specify context processors

--- vegancity/settings.py
+++ vegancity/settings.py
@@ -91,6 +91,16 @@ GLOBAL_MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
 ROOT_URLCONF = 'vegancity.urls'
 
 TEMPLATE_DIRS = (
