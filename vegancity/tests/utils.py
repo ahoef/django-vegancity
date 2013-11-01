c810eedb25d681c20e5d76c2788901776cef580f
Steve Lamb <slamb@azavea.com>
2013-11-01 15:47:22 -0400

    Update get_user test util to take user args

--- vegancity/tests/utils.py
+++ vegancity/tests/utils.py
@@ -1,6 +1,8 @@
 from vegancity.models import User
 
 
-def get_user():
-    user, _ = User.objects.get_or_create(username="Moby")
+def get_user(*args, **kwargs):
+    if 'username' not in kwargs:
+        kwargs.update({'username': 'Moby'})
+    user, _ = User.objects.get_or_create(*args, **kwargs)
     return user
